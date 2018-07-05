22fa81a23eb9c1c5d8d8b244febeb6fe2135a4aa
Nicolas Pieter <nicolaspieter@yahoo.com>
2018-07-06 02:39:53 +0700

    Added react redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+// Main app load
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// React router related imports
+import { BrowserRouter } from 'react-router-dom';
+
+// React redux related imports
+import { Provider } from 'react-redux';
+import store from './store.js';
+
+import './index.css';
+
+ReactDOM.render((
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  ), document.getElementById('root')
+);
+
 registerServiceWorker();
