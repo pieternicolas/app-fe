f3f00f3a907bfa20a8aae87bd31e502f01112667
Nicolas Pieter <nicolaspieter@yahoo.com>
2018-07-19 04:35:14 +0700

    Renamed file

--- src/components/Auth/UserIsAuthenticated.js
+++ src/components/Auth/UserIsAuthenticated.js
@@ -1,6 +1,6 @@
 import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect';
 
-const privateOnlyRoute = connectedRouterRedirect({
+const userIsAuthenticated = connectedRouterRedirect({
          // The url to redirect user to if they fail
         redirectPath: '/login',
          // If selector is true, wrapper will not redirect
@@ -10,4 +10,4 @@ const privateOnlyRoute = connectedRouterRedirect({
         wrapperDisplayName: 'UserIsAuthenticated'
 });
 
-export default privateOnlyRoute;
+export default userIsAuthenticated;
